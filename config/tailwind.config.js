b48247c103cc34958338e4859e1621e6e047106e
Torsten <torsten@rubydesign.fi>
2022-12-07 21:52:32 +0200

    fixing use of merged stylesheet

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}' ,
+    '../app/views/**/*.{erb,haml,html,slim}' ,
   ],
   theme: {
     extend: {
