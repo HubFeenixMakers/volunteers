fe7f0f4701e4b385cf1a8d05da625bd53a2611ec
Torsten <torsten@rubydesign.fi>
2023-01-08 14:59:10 +0200

    rename merged view to page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   mount Merged::Engine => "/merged"
 
-  get ":id" , to: "merged/view#view" , id: :id
-  root "merged/view#view" , id: 'index'
+  get ":id" , to: "merged/view#page" , id: :id
+  root "merged/view#page" , id: 'index'
 end
