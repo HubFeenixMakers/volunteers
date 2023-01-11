23a2254d2e8e95c091b927bbd9d9f1cad3d1af1c
Torsten <torsten@rubydesign.fi>
2023-01-11 18:48:06 +0200

    adding stories, scaffold

---
+++ db/migrate/20230111164425_create_stories.rb
@@ -0,0 +1,12 @@
+class CreateStories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stories do |t|
+      t.string :picture
+      t.string :header
+      t.text :text
+      t.date :happened
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_154221) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_164425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_31_154221) do
     t.string "unconfirmed_email"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "picture"
+    t.string "header"
+    t.text "text"
+    t.date "happened"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "thredded_categories", force: :cascade do |t|
