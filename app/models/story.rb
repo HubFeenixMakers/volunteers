3f53b6b21b202e9d816b9e061db786d4aa929061
Torsten <torsten@rubydesign.fi>
2023-01-12 14:34:26 +0200

    story validation, member link

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,9 @@ class Story < ApplicationRecord
 
   mount_uploader :picture, PictureUploader
 
+  validates :text, length: { maximum: 1000 }
+  validates :header , length: { minimum: 5 }
+
   def name
     header
   end
