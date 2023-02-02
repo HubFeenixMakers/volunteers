5ceaff890f2a147befbd9343f8d31102352aace8
Torsten <torsten@villataika.fi>
2023-02-02 17:36:17 +0200

    instatiate lightbox only when needed, slight hack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import GLightbox from 'glightbox';
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
 
-const lightbox = GLightbox({  });
+// Currently haml filter does not support modules.
+// import and make global as workaround
+//
+// haml module syntax is really clunky, filing issue and hoping
+//
+import GLightbox from 'glightbox';
+globalThis.GLightbox = GLightbox;
